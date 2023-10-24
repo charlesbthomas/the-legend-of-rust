bfc1d24f7d68bb62814bde6567173d9792f87799
Charlie Thomas <charlesbthomas94@gmail.com>
2023-10-23 22:39:34 -0500

    add: game over state

--- src/systems/combat.rs
+++ src/systems/combat.rs
@@ -2,6 +2,7 @@ use crate::prelude::*;
 
 #[system]
 #[read_component(WantsToAttack)]
+#[read_component(Player)]
 #[write_component(Health)]
 pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
     let mut attackers = <(Entity, &WantsToAttack)>::query();
@@ -10,13 +11,17 @@ pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
         .map(|(entity, attack)| (*entity, attack.victim))
         .collect();
     victims.iter().for_each(|(message, victim)| {
-        if let Ok(mut health) = ecs.entry_mut(*victim).unwrap().get_component_mut::<Health>() {
-            println!("Health before: {}", health.current);
+        let is_player = ecs.entry_ref(*victim).unwrap().get_component::<Player>().is_ok();
+
+        if let Ok(mut health) = ecs
+            .entry_mut(*victim)
+            .unwrap()
+            .get_component_mut::<Health>()
+        {
             health.current -= 1;
-            if health.current < 1 {
+            if health.current < 1 && !is_player {
                 commands.remove(*victim);
             }
-            println!("Health after: {}", health.current);
         }
         commands.remove(*message);
     });
